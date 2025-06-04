10fcf247c0b9d33658a5924ef4dbf7d2f6679a61
guilherme <guilherme.evangelista@estudantes.ifg.edu.br>
2025-06-03 22:05:19 -0300

    programa leitor codigo

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('hello world')
+lista = []
+
+escolha = 0
+cout = 0
+while(escolha != 4):    
+    print("escolha uma opção:")
+    print("digite 1 para adcionar nova tarefa")
+    print("digite 2 para listar todas as tarefas")
+    print("digite 3 para marcar tarefa como concluida")
+    print("digite 4 para sair")
+    
+    escolha = int(input())
+    if(escolha == 1):
+        programa = {
+            "codigo": " ",
+            "tarefa": " ",
+            "descrição": " ",
+            "status": "pendente"
+        }
+        cout = cout +1
+        print("escreva a descrição da nova tarefa")
+        programa["tarefa"] = input()
+        programa["codigo"] = cout
+        lista.append(programa)
+        
+    if(escolha == 2):
+        print(lista)
+        
+    if(escolha == 3):
+        print("digite o id da tarefa concluida")
+        aux = int (input())
+        for l in lista:
+            if l["codigo"] == aux:
+                l["status"] = "concluido" 
+                print("status da tarefa alterado para concluido")
+        
+    
